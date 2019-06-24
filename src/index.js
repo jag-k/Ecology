d066b479a54d3bed72561ae578554fd7ebb92221
defaultxreal <357484665ruS>
2019-06-24 12:47:45 +0300

    Added parcel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-const vk = require('@vkontakte/vkui-connect-promise');
+import connect from '@vkontakte/vkui-connect-promise';
 
-console.log();
-
-vk.default.send('VKWebAppInit', {}) 
-  .then(data => {
-      console.log(`Test: ${data}`);
-  }) //Обработка события EventNameResult в случае успеха 
-  .catch(error => {
-      console.log(`Error: ${error}`);
-  }); //Обработка события EventNameFailed в случае ошибки
+(async () => {
+    try {
+        // Initialize web app
+        await connect.send('VKWebAppInit', {});
+    } catch (e) {
+        console.error(`Error initialize web app: ${e}`);
+    }
+})();
