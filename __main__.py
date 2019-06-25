07691147050f8edf9da49613dd7a20fa958af3c9
Jag_k <admin@jag-k.info>
2019-06-25 11:46:20 +0300

    Added main page

--- __main__.py
+++ __main__.py
@@ -1,10 +1,26 @@
+from pprint import pprint
 import bottle
+from bottle import static_file, redirect, request, response
 from modules import *
 
 app = bottle.Bottle()
 api = API('api')
 
 api.add_method(GET, get_coords)
+
+
+@app.route('/')
+def main_page():
+    pprint(request.json)
+    return static_file('index.html', 'dist')
+
+
+@app.route('/<file:path>')
+def static(file):
+    if file == 'index.html':
+        return bottle.HTTPError(404)
+    return static_file(file, 'dist')
+
 
 if __name__ == '__main__':
     api.connect(app)
